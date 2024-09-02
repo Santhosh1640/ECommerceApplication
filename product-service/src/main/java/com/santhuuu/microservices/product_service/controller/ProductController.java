e459a70f2f33a280ae9508a5012052778c618ab0
santhosh <hemanthsanthosh123@gmail.com>
2024-09-02 10:14:26 +0530

    Added Swagger for documenting REST APIs

--- product-service/src/main/java/com/santhuuu/microservices/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/santhuuu/microservices/product_service/controller/ProductController.java
@@ -7,12 +7,11 @@ import com.santhuuu.microservices.product_service.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@Controller
-@RequestMapping("api/product")
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
 
     @Autowired
