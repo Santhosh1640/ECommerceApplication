d405569b0ac334b14f74b61ac3c375b577ac2fee
santhosh <hemanthsanthosh123@gmail.com>
2024-09-02 16:47:39 +0530

    Adding Circuit Breaker using Resilience4j in Order Service

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ECommerceApplication
 
-DOCKER COMPOSE SETUP FOR POSTGRESQL
+## DOCKER COMPOSE SETUP FOR POSTGRESQL
 
 1) Create docker-compose file, run "docker-compose up"
 2) Image will be created.
@@ -9,3 +9,25 @@ DOCKER COMPOSE SETUP FOR POSTGRESQL
 5) Copy the IP address
 6) Go to localhost:/5050 (PgAdmin)
 7) Add new server
+
+
+This command will reapply the line-ending normalization rules to all files in your repository, 
+adjusting line endings to match the rules specified in your .gitattributes file or by your 
+core.autocrlf setting.
+
+-> git add --renormalize .
+-> git commit -m "Apply LF line endings to Java files"
+
+
+## SECURITY:
+### KeyCloak
+We use KeyCloak to secure API Gateway
+
+1) Create a docker-compose file in api-gatewayy service for keycloak
+2) Go to localhost://8181
+3) Create a Realm
+Realm: Ream is a container which contains
+
+## DOCUMENTING REST API - OPEN API
+### Swagger
+It is a tool which uses open api specification and provides a library to document rest api
--- order-service/src/main/java/com/santhuuu/microservices/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/santhuuu/microservices/order_service/client/InventoryClient.java
@@ -1,14 +1,29 @@
 package com.santhuuu.microservices.order_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 //@FeignClient(name= "inventory-service", url = "http://localhost:8082")
+
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
 //    @GetMapping("/api/inventory")
     //Instead of using Feign Client here, we used RestClient
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity);
+
+    default boolean fallbackMethod(String code, int quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", code, throwable.getMessage());
+        return false;
+    }
 
 }
