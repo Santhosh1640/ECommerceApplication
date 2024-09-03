4b949a99363db0ef4dd2f8f5dac888bdf2bf3099
santhosh <hemanthsanthosh123@gmail.com>
2024-09-03 15:05:45 +0530

    Adding API-Gateway microservice

---
+++ api-gatewayy/src/main/java/com/santhuuu/microservices/api_gatewayy/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.santhuuu.microservices.api_gatewayy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200") // Angular app URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
---
+++ api-gatewayy/src/test/java/com/santhuuu/microservices/api_gatewayy/ApiGatewayyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhuuu.microservices.api_gatewayy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
